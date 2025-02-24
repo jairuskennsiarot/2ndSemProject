a02d5532f09614a134a5f026231c37ed4d085b84
jairuskennsiarot <jairusdesucatansiarot@gmail.com>
2025-02-24 09:42:47 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// function btn(){
+//     const text = document.getElementById("h1");
+//     text.textContent = "nAtUg Ka"
+//     text.style.color = "#5c65c0"
+//     text.style.textAlign = "center"
+//     text.style.background = "#301c41"
+//     text.style.padding = "100px"
+//     text.style.borderRadius = "500px"
+//     document.body.style.background = "#1c0b2b"
+// }
+
+// const Big = document.getElementById("big");
+// Big.style.fontSize = "50px"
+
+
+function btnClick(){
+    const textBox = document.getElementById("myInput").value;
+    const textBoxx = alert(textBox)
+}
